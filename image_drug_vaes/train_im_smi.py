70555c9a1014bb58bc71c757423c9af84950dfd1
Austin Clyde <aclyde@uchicago.edu>
2021-03-15 11:34:59 -0500

    fixed spelling error

--- image_drug_vaes/train_im_smi.py
+++ image_drug_vaes/train_im_smi.py
@@ -110,188 +110,181 @@ if config['distributed']:
 
 
 def train(epoch):
-    with experiment.train():
+    print("Epoch {}: batch_size {}".format(epoch, config['batch_size']))
+    decoder.train()  # train mode (dropout and batchnorm is used)
+    encoder.train()
+    total_losses = AverageMeter()  # loss (per word decoded)
+    total_per_char_acc = AverageMeter()
+    total_string_acc = AverageMeter()
+    total_editDist = AverageMeter()
+
+    for batch_idx, (embed, data, embedlen) in enumerate(train_loader_food):
+        imgs_orig = data.float()
+        if config['distributed']:
+            imgs_orig = imgs_orig.cuda(1)
+            caps = embed.cuda(2)
+            caplens = embedlen.cuda(2).view(-1, 1)
+
+        # Forward prop.
+        imgs_vae = encoder(imgs_orig)
+
+        if config['distributed']:
+            imgs_vae = imgs_vae.cuda(2)
+
+        scores, caps_sorted, decode_lengths, alphas, sort_ind = decoder(imgs_vae, caps, caplens,
+                                                                        teacher_forcing=bool(epoch < 3))
+
+        # Since we decoded starting with <start>, the targets are all words after <start>, up to <end>
+        imgs_vae = imgs_vae[sort_ind]
+        imgs_orig = imgs_orig[sort_ind]
+        targets = caps_sorted[:, 1:]
+        scores_copy = scores.clone()
+        targets_copy = targets.clone()
+
+        scores, _ = pack_padded_sequence(scores, decode_lengths, batch_first=True)
+        targets, _ = pack_padded_sequence(targets, decode_lengths, batch_first=True)
+
+        # Calculate loss
+        loss = criterion(scores, targets)
+
+        # Add doubly stochastic attention regularization
+        loss += config['alpha_c'] * ((1. - alphas.sum(dim=1)) ** 2).mean()
+
+        # Back prop.
+        decoder_optimizer.zero_grad()
+        if encoder_optimizer is not None:
+            encoder_optimizer.zero_grad()
+        loss.backward()
+
+        # Clip gradients
+        if config['grad_clip'] is not None:
+            clip_gradient(decoder_optimizer, config['grad_clip'])
+            if encoder_optimizer is not None:
+                clip_gradient(encoder_optimizer, config['grad_clip'])
+
+        # Update weights
+        decoder_optimizer.step()
+        if encoder_optimizer is not None:
+            encoder_optimizer.step()
+
+        # Keep track of metrics
+        total_losses.update(loss.item(), sum(decode_lengths))
+
+        # acc_per_c
+        acc_c = torch.max(scores, dim=1)[1].eq(targets).sum().item() / float(targets.shape[0])
+        total_per_char_acc.update(acc_c)
+
+        # acc_per_string
+        _, preds = torch.max(scores_copy, dim=2)
+        preds = preds.cpu().numpy()
+        targets_copy = targets_copy.cpu().numpy()
+        for i in range(preds.shape[0]):
+            s1 = preds[i, ...]
+            s2 = targets_copy[i, ...]
+            s1 = "".join([charset[chars] for chars in s1]).strip()
+            s2 = "".join([charset[chars] for chars in s2]).strip()
+            total_string_acc.update(1.0 if s1 == s2 else 0.0)
+            total_editDist.update(levenshteinDistance(s1, s2))
+
+            # if len(corrects) < 50 and s1 == s2:
+            #     a = add_text_to_image(imgs_orig[i, ...], s1, "orig")
+            #     corrects.append(a)
+            #     a = add_text_to_image(imgs_vae[i, ...], s2, "vae", str(0))
+            #     corrects.append(a)
+            #
+            # if len(wrongs) < 50 and s1 != s2:
+            #     dist = levenshteinDistance(s1, s2)
+            #     s2 = s2
+            #     a = add_text_to_image(imgs_orig[i, ...], s1, "orig")
+            #     wrongs.append(a)
+            #     a = add_text_to_image(imgs_vae[i, ...], s2, "vae", str(dist))
+            #     wrongs.append(a)
+
         if config['use_comet']:
-            experiment.log_current_epoch(epoch)
+            experiment.log_metric('loss', loss.item() / sum(decode_lengths))
+            experiment.log_metric("acc_per_char", total_per_char_acc.avg)
+            experiment.log_metric("acc_per_string", total_string_acc.avg)
+            experiment.log_metric("editDist", total_editDist.avg)
 
-        print("Epoch {}: batch_size {}".format(epoch, config['batch_size']))
-        decoder.train()  # train mode (dropout and batchnorm is used)
-        encoder.train()
-        total_losses = AverageMeter()  # loss (per word decoded)
-        total_per_char_acc = AverageMeter()
-        total_string_acc = AverageMeter()
-        total_editDist = AverageMeter()
+        if batch_idx % config['log_interval'] == 0:
+            # print("wrongs len: {}, correct len: {}".format(len(wrongs), len(corrects)))
+            print(
+                'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tAccPerChar: {:.6f}\tAccPerStr: {:.6f}\teditDist: {:.6f} {}'.format(
+                    epoch, batch_idx * len(train_loader_food), len(train_loader_food.dataset),
+                           100. * batch_idx / len(train_loader_food),
+                    total_losses.avg,
+                    total_per_char_acc.avg,
+                    total_string_acc.avg,
+                    total_editDist.avg,
+                    datetime.datetime.now()))
 
-        for batch_idx, (embed, data, embedlen) in enumerate(train_loader_food):
-            imgs = data.float()
-            if config['distributed']:
-                imgs = imgs.cuda(1)
-                caps = embed.cuda(2)
-                caplens = embedlen.cuda(2).view(-1, 1)
-
-            # Forward prop.
-            imgs = encoder(imgs)
-
-            if config['distributed']:
-                imgs = imgs.cuda(2)
-
-            scores, caps_sorted, decode_lengths, alphas, sort_ind = decoder(imgs, caps, caplens,
-                                                                            teacher_forcing=bool(epoch < 3))
-
-            # Since we decoded starting with <start>, the targets are all words after <start>, up to <end>
-            imgs_vae = imgs_vae[sort_ind]
-            imgs_orig = imgs_orig[sort_ind]
-            targets = caps_sorted[:, 1:]
-            scores_copy = scores.clone()
-            targets_copy = targets.clone()
-
-            scores, _ = pack_padded_sequence(scores, decode_lengths, batch_first=True)
-            targets, _ = pack_padded_sequence(targets, decode_lengths, batch_first=True)
-
-            # Calculate loss
-            loss = criterion(scores, targets)
-
-            # Add doubly stochastic attention regularization
-            loss += config['alpha_c'] * ((1. - alphas.sum(dim=1)) ** 2).mean()
-
-            # Back prop.
-            decoder_optimizer.zero_grad()
-            if encoder_optimizer is not None:
-                encoder_optimizer.zero_grad()
-            loss.backward()
-
-            # Clip gradients
-            if config['grad_clip'] is not None:
-                clip_gradient(decoder_optimizer, config['grad_clip'])
-                if encoder_optimizer is not None:
-                    clip_gradient(encoder_optimizer, config['grad_clip'])
-
-            # Update weights
-            decoder_optimizer.step()
-            if encoder_optimizer is not None:
-                encoder_optimizer.step()
-
-            # Keep track of metrics
-            total_losses.update(loss.item(), sum(decode_lengths))
-
-            # acc_per_c
-            acc_c = torch.max(scores, dim=1)[1].eq(targets).sum().item() / float(targets.shape[0])
-            total_per_char_acc.update(acc_c)
-
-            # acc_per_string
-            _, preds = torch.max(scores_copy, dim=2)
-            preds = preds.cpu().numpy()
-            targets_copy = targets_copy.cpu().numpy()
-            for i in range(preds.shape[0]):
-                s1 = preds[i, ...]
-                s2 = targets_copy[i, ...]
-                s1 = "".join([charset[chars] for chars in s1]).strip()
-                s2 = "".join([charset[chars] for chars in s2]).strip()
-                total_string_acc.update(1.0 if s1 == s2 else 0.0)
-                total_editDist.update(levenshteinDistance(s1, s2))
-
-                # if len(corrects) < 50 and s1 == s2:
-                #     a = add_text_to_image(imgs_orig[i, ...], s1, "orig")
-                #     corrects.append(a)
-                #     a = add_text_to_image(imgs_vae[i, ...], s2, "vae", str(0))
-                #     corrects.append(a)
-                #
-                # if len(wrongs) < 50 and s1 != s2:
-                #     dist = levenshteinDistance(s1, s2)
-                #     s2 = s2
-                #     a = add_text_to_image(imgs_orig[i, ...], s1, "orig")
-                #     wrongs.append(a)
-                #     a = add_text_to_image(imgs_vae[i, ...], s2, "vae", str(dist))
-                #     wrongs.append(a)
-
-            if config['use_comet']:
-                experiment.log_metric('loss', loss.item() / sum(decode_lengths))
-                experiment.log_metric("acc_per_char", total_per_char_acc.avg)
-                experiment.log_metric("acc_per_string", total_string_acc.avg)
-                experiment.log_metric("editDist", total_editDist.avg)
-
-            if batch_idx % config['log_interval'] == 0:
-                # print("wrongs len: {}, correct len: {}".format(len(wrongs), len(corrects)))
-                print(
-                    'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tAccPerChar: {:.6f}\tAccPerStr: {:.6f}\teditDist: {:.6f} {}'.format(
-                        epoch, batch_idx * len(train_loader_food), len(train_loader_food.dataset),
-                               100. * batch_idx / len(train_loader_food),
-                        total_losses.avg,
-                        total_per_char_acc.avg,
-                        total_string_acc.avg,
-                        total_editDist.avg,
-                        datetime.datetime.now()))
 
 
 def test(epoch):
-    with experiment.test():
+    print("Epoch {}: batch_size {}".format(epoch, config['batch_size']))
+    decoder.train()  # train mode (dropout and batchnorm is used)
+    encoder.train()
+    total_losses = AverageMeter()  # loss (per word decoded)
+    total_per_char_acc = AverageMeter()
+    total_string_acc = AverageMeter()
+    total_editDist = AverageMeter()
+
+    for batch_idx, (embed, data, embedlen) in enumerate(train_loader_food):
+        imgs = data.float()
+        if config['distributed']:
+            imgs = imgs.cuda(1)
+            caps = embed.cuda(2)
+            caplens = embedlen.cuda(2).view(-1, 1)
+
+        # Forward prop.
+        imgs = encoder(imgs)
+
+        if config['distributed']:
+            imgs = imgs.cuda(2)
+
+        scores, caps_sorted, decode_lengths, alphas, sort_ind = decoder(imgs, caps, caplens,
+                                                                        teacher_forcing=bool(epoch < 3))
+
+        # Since we decoded starting with <start>, the targets are all words after <start>, up to <end>
+        imgs_vae = imgs_vae[sort_ind]
+        imgs_orig = imgs_orig[sort_ind]
+        targets = caps_sorted[:, 1:]
+        scores_copy = scores.clone()
+        targets_copy = targets.clone()
+
+        scores, _ = pack_padded_sequence(scores, decode_lengths, batch_first=True)
+        targets, _ = pack_padded_sequence(targets, decode_lengths, batch_first=True)
+
+        # Calculate loss
+        loss = criterion(scores, targets)
+
+        # Add doubly stochastic attention regularization
+        loss += config['alpha_c'] * ((1. - alphas.sum(dim=1)) ** 2).mean()
+
+        # Keep track of metrics
+        total_losses.update(loss.item(), sum(decode_lengths))
+
+        # acc_per_c
+        acc_c = torch.max(scores, dim=1)[1].eq(targets).sum().item() / float(targets.shape[0])
+        total_per_char_acc.update(acc_c)
+
+        # acc_per_string
+        _, preds = torch.max(scores_copy, dim=2)
+        preds = preds.cpu().numpy()
+        targets_copy = targets_copy.cpu().numpy()
+        for i in range(preds.shape[0]):
+            s1 = preds[i, ...]
+            s2 = targets_copy[i, ...]
+            s1 = "".join([charset[chars] for chars in s1]).strip()
+            s2 = "".join([charset[chars] for chars in s2]).strip()
+            total_string_acc.update(1.0 if s1 == s2 else 0.0)
+            total_editDist.update(levenshteinDistance(s1, s2))
+
         if config['use_comet']:
-            experiment.log_current_epoch(epoch)
-
-        print("Epoch {}: batch_size {}".format(epoch, config['batch_size']))
-        decoder.train()  # train mode (dropout and batchnorm is used)
-        encoder.train()
-        total_losses = AverageMeter()  # loss (per word decoded)
-        total_per_char_acc = AverageMeter()
-        total_string_acc = AverageMeter()
-        total_editDist = AverageMeter()
-
-        for batch_idx, (embed, data, embedlen) in enumerate(train_loader_food):
-            imgs = data.float()
-            if config['distributed']:
-                imgs = imgs.cuda(1)
-                caps = embed.cuda(2)
-                caplens = embedlen.cuda(2).view(-1, 1)
-
-            # Forward prop.
-            imgs = encoder(imgs)
-
-            if config['distributed']:
-                imgs = imgs.cuda(2)
-
-            scores, caps_sorted, decode_lengths, alphas, sort_ind = decoder(imgs, caps, caplens,
-                                                                            teacher_forcing=bool(epoch < 3))
-
-            # Since we decoded starting with <start>, the targets are all words after <start>, up to <end>
-            imgs_vae = imgs_vae[sort_ind]
-            imgs_orig = imgs_orig[sort_ind]
-            targets = caps_sorted[:, 1:]
-            scores_copy = scores.clone()
-            targets_copy = targets.clone()
-
-            scores, _ = pack_padded_sequence(scores, decode_lengths, batch_first=True)
-            targets, _ = pack_padded_sequence(targets, decode_lengths, batch_first=True)
-
-            # Calculate loss
-            loss = criterion(scores, targets)
-
-            # Add doubly stochastic attention regularization
-            loss += config['alpha_c'] * ((1. - alphas.sum(dim=1)) ** 2).mean()
-
-            # Keep track of metrics
-            total_losses.update(loss.item(), sum(decode_lengths))
-
-            # acc_per_c
-            acc_c = torch.max(scores, dim=1)[1].eq(targets).sum().item() / float(targets.shape[0])
-            total_per_char_acc.update(acc_c)
-
-            # acc_per_string
-            _, preds = torch.max(scores_copy, dim=2)
-            preds = preds.cpu().numpy()
-            targets_copy = targets_copy.cpu().numpy()
-            for i in range(preds.shape[0]):
-                s1 = preds[i, ...]
-                s2 = targets_copy[i, ...]
-                s1 = "".join([charset[chars] for chars in s1]).strip()
-                s2 = "".join([charset[chars] for chars in s2]).strip()
-                total_string_acc.update(1.0 if s1 == s2 else 0.0)
-                total_editDist.update(levenshteinDistance(s1, s2))
-
-            if config['use_comet']:
-                experiment.log_metric('loss', loss.item() / sum(decode_lengths))
-                experiment.log_metric("acc_per_char", total_per_char_acc.avg)
-                experiment.log_metric("acc_per_string", total_string_acc.avg)
-                experiment.log_metric("editDist", total_editDist.avg)
+            experiment.log_metric('loss', loss.item() / sum(decode_lengths))
+            experiment.log_metric("acc_per_char", total_per_char_acc.avg)
+            experiment.log_metric("acc_per_string", total_string_acc.avg)
+            experiment.log_metric("editDist", total_editDist.avg)
 
         return total_losses.avg
 
